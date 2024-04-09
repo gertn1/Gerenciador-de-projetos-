165728064eff0c0d23c2029f1d5f4ecdc603b55e
Germano Ribeiro <germanno.verde@gmail.com>
2024-04-08 22:16:37 -0300

    tela de projetos foi acresentada botão editar e adicionar serviço

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Container from './components/layout/Container';
 import NavBar from './components/layout/NavBar';
 import Footer from './components/layout/Footer';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 function App() {
   return (
@@ -15,11 +16,13 @@ function App() {
       <NavBar />
       
       <Routes>
-        <Route path="/" element={<Container customClass="min-height"> <Home /> </Container>}/>
+        <Route exact path="/" element={<Container customClass="min-height"> <Home /> </Container>}/>
         <Route path="/company"element={<Container customClass="min-height"><Company /></Container>}/>
         <Route path="/projects"element={<Container customClass="min-height"><Projects/></Container>}/>
         <Route path="/contact"element={<Container customClass="min-height"><Contact /></Container>}/>
         <Route path="/newproject"element={<Container customClass="min-height"><NewProject /></Container>}/>
+        <Route path="/project/:id"element={<Container customClass="min-height"><Project /></Container>}/>
+
       </Routes>
      <Footer />
     </Router>
